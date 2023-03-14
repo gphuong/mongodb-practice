848e180b65d81ae7e6548a8835a107b985e3adcf
Phuong Nguyen <phuong.nguyen@aeris.net>
2023-03-15 00:15:54 +0700

    #ref add unit test

--- src/test/java/com/phuongheh/converters/RecipeToRecipeCommandTest.java
+++ src/test/java/com/phuongheh/converters/RecipeToRecipeCommandTest.java
@@ -1,17 +1,13 @@
 package com.phuongheh.converters;
 
-import com.phuongheh.commands.CategoryCommand;
-import com.phuongheh.commands.IngredientCommand;
-import com.phuongheh.commands.NotesCommand;
 import com.phuongheh.commands.RecipeCommand;
-import com.phuongheh.domain.Difficulty;
-import com.phuongheh.domain.Recipe;
+import com.phuongheh.domain.*;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class RecipeCommandToRecipeTest {
+public class RecipeToRecipeCommandTest {
     public static final String RECIPE_ID = "1";
     public static final Integer COOK_TIME = Integer.valueOf("5");
     public static final Integer PREP_TIME = Integer.valueOf("7");
@@ -25,30 +21,31 @@ public class RecipeCommandToRecipeTest {
     public static final String CAT_ID_2 = "2";
     public static final String INGRED_ID_1 = "3";
     public static final String INGRED_ID_2 = "4";
-    public static final String NOTES_ID = "4";
-
-    RecipeCommandToRecipe converter;
+    public static final String NOTES_ID = "9";
+    RecipeToRecipeCommand converter;
 
     @Before
     public void setUp() {
-        converter = new RecipeCommandToRecipe(new CategoryCommandToCategory(),
-                new IngredientCommandToIngredient(new UnitOfMeasureCommandToUnitOfMeasure()),
-                new NotesCommandToNotes());
+        converter = new RecipeToRecipeCommand(
+                new CategoryToCategoryCommand(),
+                new IngredientToIngredientCommand(new UnitOfMeasureToUnitOfMeasureCommand()),
+                new NotesToNotesCommand()
+        );
     }
 
     @Test
-    public void testNullObject(){
+    public void testNullObject() {
         assertNull(converter.convert(null));
     }
 
     @Test
-    public void testEmptyObject(){
-        assertNotNull(converter.convert(new RecipeCommand()));
+    public void testEmptyObject() {
+        assertNotNull(converter.convert(new Recipe()));
     }
 
     @Test
-    public void convert(){
-        RecipeCommand recipeCommand = new RecipeCommand();
+    public void convert() {
+        Recipe recipeCommand = new Recipe();
         recipeCommand.setId(RECIPE_ID);
         recipeCommand.setCookTime(COOK_TIME);
         recipeCommand.setPrepTime(PREP_TIME);
@@ -59,30 +56,30 @@ public class RecipeCommandToRecipeTest {
         recipeCommand.setSource(SOURCE);
         recipeCommand.setUrl(URL);
 
-        NotesCommand notes = new NotesCommand();
+        Notes notes = new Notes();
         notes.setId(NOTES_ID);
 
         recipeCommand.setNotes(notes);
 
-        CategoryCommand category = new CategoryCommand();
+        Category category = new Category();
         category.setId(CAT_ID_1);
 
-        CategoryCommand category2 = new CategoryCommand();
+        Category category2 = new Category();
         category2.setId(CAT_ID_2);
 
         recipeCommand.getCategories().add(category);
         recipeCommand.getCategories().add(category2);
 
-        IngredientCommand ingredient = new IngredientCommand();
+        Ingredient ingredient = new Ingredient();
         ingredient.setId(INGRED_ID_1);
 
-        IngredientCommand ingredient2 = new IngredientCommand();
+        Ingredient ingredient2 = new Ingredient();
         ingredient2.setId(INGRED_ID_2);
 
         recipeCommand.getIngredients().add(ingredient);
         recipeCommand.getIngredients().add(ingredient2);
 
-        Recipe recipe = converter.convert(recipeCommand);
+        RecipeCommand recipe = converter.convert(recipeCommand);
         assertNotNull(recipe);
         assertEquals(RECIPE_ID, recipe.getId());
         assertEquals(COOK_TIME, recipe.getCookTime());
---
+++ src/test/java/com/phuongheh/converters/UnitOfMeasureToUnitOfMeasureCommandTest.java
@@ -0,0 +1,4 @@
+package com.phuongheh.converters;
+
+public class UnitOfMeasureToUnitOfMeasureCommandTest {
+}
